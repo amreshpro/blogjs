3354e22fcf541bd13c5b1ab5a785eb03b24661ad
Amresh Maurya <amresh.terminal@gmail.com>
2024-10-11 23:25:31 +0530

    swagger setup and fixed issue

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,58 @@
-import express from "express";
-import loggerMiddleware from "./middleware/logger";
-import { globalErrorHandler } from "./middleware/errors";
+import swaggerUi from "swagger-ui-express";
+import express, { urlencoded, type NextFunction } from "express";
+import swaggerSpec from "./config/swagger";
 
 const app = express();
 
 // middleware
 
-app.use(express.json())
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
 // app.use((req,res,next)=>globalErrorHandler(req,res,next))
 
-app.get("/", (req, res) => {
+// Swagger UI route
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+/**
+ * @swagger
+ * /:
+ * get:
+ * summary: hello
+ * description: hello description api
+ */
+app.get("/", (_req, res) => {
   res.send("Hello World!");
+});
+
+/**
+ * @swagger
+ * /api/v1/users:
+ *   get:
+ *     summary: Retrieve a list of users
+ *     description: Retrieve a list of users from the system.
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     example: John Doe
+ */
+app.get("/users", (_req, res) => {
+  const users = [
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Jane Doe" },
+  ];
+  res.json(users); // Directly sending response, no need to return Response
 });
 
 export default app;
---
+++ src/config/swagger.ts
@@ -0,0 +1,23 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+// Swagger definition object
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0", // Specify the OpenAPI version
+    info: {
+      title: "Express API Documentation",
+      version: "1.0.0",
+      description: "API documentation for my Express.js project",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000", // Replace with your server URL
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts", "./src/models/*.ts", "./src/*.ts"], // Path to your API docs (where your JSDoc comments are)
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+export default swaggerSpec;

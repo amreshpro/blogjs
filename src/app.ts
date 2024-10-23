b33c13ef914c73b6a0270951cd224f735c178a6a
Amresh Maurya <amresh.terminal@gmail.com>
2024-10-23 23:27:02 +0530

    add security package and fix build issue

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { urlencoded } from "express";
+import expressRateLimit from "express-rate-limit";
 import swaggerUi from "swagger-ui-express";
 import loggerMiddleware from "./middleware/logger";
 import globalErrorHandler from "./middleware/errors";
@@ -8,6 +9,7 @@ import authRouter from "./routes/auth";
 import path from "path";
 import cors from "cors";
 import swagger from "./docs/swagger.json";
+import helmet from "helmet";
 
 const app = express();
 
@@ -25,6 +27,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(cors());
 
 // Routes
+
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/posts", postRouter);
 app.use("/api/v1/auth", authRouter);
@@ -37,6 +40,14 @@ app.get("/", (req, res, next) => {
   next();
 });
 
+const limiter = expressRateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
+
+app.use(helmet());
 // global error handler ( must be after routes)
 app.use(globalErrorHandler);
 
